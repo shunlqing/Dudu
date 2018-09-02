a046430ad24b8af1038ff41abdace5c59a1aa9d7
shunlqing <shunlqing@163.com>
2018-09-03 00:09:41 +0800

    update

--- benchmark/multi_thread.sh
+++ benchmark/multi_thread.sh
@@ -8,9 +8,9 @@ for nosessions in 100 1000 10000; do
   for nothreads in 1 2 3 4; do
     sleep 2
     echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-    taskset -c 0 ./../cmake-build-debug/Dudu $nothreads & srvpid=$!
+    taskset -c 0 ../Dudu $nothreads & srvpid=$!
     sleep 1
-    taskset -c 1 ./../cmake-build-debug/cli 127.0.0.1 8088 $nothreads $bufsize $nosessions $timeout
+    taskset -c 1 ../cli 127.0.0.1 8088 $nothreads $bufsize $nosessions $timeout
     kill -9 $srvpid
   done
 done
--- benchmark/single_thread.sh
+++ benchmark/single_thread.sh
@@ -10,9 +10,9 @@ nothreads=1
 for bufsize in 1024 2048 4096 8192 16384 81920; do
 for nosessions in 1 10 100 1000 10000; do
   echo "==> Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-  taskset -c 0 ./../cmake-build-debug/Dudu $nothreads & srvpid=$!
+  taskset -c 0 ../Dudu $nothreads & srvpid=$!
   sleep 1
-  taskset -c 1 ./../cmake-build-debug/cli 127.0.0.1 8088 $nothreads $bufsize $nosessions $timeout
+  taskset -c 1 ../cli 127.0.0.1 8088 $nothreads $bufsize $nosessions $timeout
   kill -9 $srvpid
   sleep 5
 done
